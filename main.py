e7d0ebd3506125c2f90cf1b2762bb06bb54e40cf
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-11-02 15:56:04 -0700

    added command line argument to toggle between windo and fullscreen modes when running

--- main.py
+++ main.py
@@ -15,8 +15,12 @@ class TooManyEddies:
     def __init__(self):
         pygame.init()
         self.settings = Settings()
-        #self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
-        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+
+        if len(sys.argv) >= 2:
+            self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        else:
+            self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+
         self.screen_size = self.screen.get_rect().size
         pygame.display.set_caption("Too Many Eddies: A Frasier Parody")
 
